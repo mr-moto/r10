047a5dde9305422e798b6873c1ec7933bf792c98
mr-moto <khash615@gmail.com>
2017-11-21 01:48:43 -0800

    init commit

---
+++ js/App.js
@@ -0,0 +1,16 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from "react";
+import About from "./scenes/About/";
+
+class App extends Component<{}> {
+  render() {
+    return <h1 id="title">Hello, world!</h1>
+  }
+}
+
+export default App;
